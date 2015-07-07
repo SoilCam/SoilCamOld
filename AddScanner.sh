340a18d0b588ff87f6818986e1708e014cda7bad
Josh Williams <joshdont@gmail.com>
2015-07-06 23:27:41 -0400

    Added add and remove functionality to AddScanner.sh. Added baseScr variable to locations.cfg file

--- AddScanner.sh
+++ AddScanner.sh
@@ -2,59 +2,97 @@
 #This script should build the files needed for you to add another scanner.
 #additional modification will likely be needed ; )
 source ~/SoilCam/processFiles/locations.cfg
-NP=1
 
 if [ $# -lt 2 ]
 then
 	echo -e "Usage: -A (Add) or -R (Remove), followed by an 8 character directory name\n \
 	example: './AddScanner.sh -A soil' will add files and config settings using the name 'soil'"
 	exit 1;
-fi
-if [ -z $1 ]
-then
-	echo "Specify -A or -R to add or remove everything related to a scan."
-	exit 1;
-fi
-
-if [ -z $2 ]
-then
-	echo "Specify a name, please keep it short and simple (8 characters. Only letters, numbers, hyphens and underscores please"
-	exit 1;
+else
+	if ! [[ "$2" =~ [^a-zA-Z0-9\_\-] ]];
+	then
+		[ $1 == "-A" ] && NP=2
+		[ $1 == "-R" ] && NP=3
+	else
+		echo "The term '$2' you used must only contain letters, numbers, hyphens and underscores. No other funny business allowed"
+		exit 1;
+	fi
 fi
 
-while [ $NP -ne 0 ];
+while [ $NP -ne 10 ];
 do
-	case $NP in
-	1) CP=$NP
-		[ $1 == "-A" ] && NP=2 echo "We are going to add files and directories for under the name '$2'"
-		[ $1 == "-R" ] && NP=3 echo "We are going to remove files and directories for under the name '$2'"
-	;;
-	2) CP=$NP
-		echo "Now we add files!
-		#new_directory=$1
-		#new_prefix="${1:0:2}_"
+	case "$NP" in
+		1)	CP=$NP
+		;;
 
-		#echo "Directory will be: $new_directory"
-		#echo "Prefix will be: $new_prefix"
+		2) 	CP=$NP
+			read -p "You want to create the image and video directory $2, as well as config files to process images into video? (Yy/Nn)" -n 1 -r
+			echo ""
+			if [[ $REPLY =~ ^[Yy]$ ]]
+			then
+				read -p "If there is already a directory named '$2', it and all its contents will be overwritten. Are you okay with this? (Yy/Nn)" -n 1 -r
+				echo ""
+				if [[ $REPLY =~ ^[Yy]$ ]]
+				then
+					echo "Okay, processing..."
+					NP=5
+				else
+					echo "Exiting"
+					exit 1;
+				fi
+			else
+				echo "Exiting"
+				exit 1;
+			fi
+		;;
+		3) 	CP=$NP
+			read -p "Are you sure you wish to DELETE $2 associated files? (Yy/Nn) " -n 1 -r
+			if [[ $REPLY =~ ^[Yy]$ ]]
+				echo ""
+			then
+				read -p "We are about to delete the config file, scripts, images and any videos associated with $2, are you sure? (Yy/Nn)" -n 1 -r
+				if [[ $REPLY =~ ^[Yy]$ ]]
+				echo ""
+				then
+					NP=4
+					echo "Proceeding to delete all of the files"
+				fi
+			else
+				echo "Yay, nothing will be deleted, exiting!"
+				exit 1;
+			fi
+		;;
+		4) 	CP=$NP
+			new_directory=$2
+			new_prefix="${2:0:2}_"
 
-		#mkdir $baseVid/$new_directory
-		#mkdir $baseImg/$new_directory
+			echo -e "Deleting directory:\t$baseImg/$new_directory/"
+			echo -e "Deleting directory:\t$baseVid/$new_directory/"
+			echo -e "Removing Image and Video file Prefix:\t$new_prefix"
+			echo -e "Deleting config file:\t$baseScr/PI_$new_directory.cfg"
+			read -p pause
+			rm -rf $baseVid/$new_directory
+			rm -rf $baseImg/$new_directory
+			rm -rf $baseScr/PI_$new_directory.cfg
+			NP=10
+		;;
+		5) 	CP=$NP #add files
+			new_directory=$2
+			new_prefix="${2:0:2}_"
 
-		#rep_directory=$new_directory rep_prefix=$new_prefix envsubst < ~/SoilCam/processFiles/PI_template.cfg >> ~/SoilCam/processFiles/PI_$new_directory.cfg
-		NP=0
-	;;
-	3) CP=$NP
-		echo "Now we delete files! Are you sure we want to delete files!?
-		read -p pause
-		NP=0
-	;;
-	0) CP=$NP
-	;;
+			echo -e "Adding directory:\t$baseImg/$new_directory/"
+			echo -e "Adding directory:\t$baseVid/$new_directory/"
+			echo -e "Image and Video file Prefix will be:\t$new_prefix"
+			echo -e "Adding new config file:\t$baseScr/PI_$new_directory.cfg"
+			read -p pause
+			mkdir $baseVid/$new_directory
+			mkdir $baseImg/$new_directory
+
+			rep_directory=$new_directory rep_prefix=$new_prefix envsubst < $baseScr/PI_template.cfg >> $baseScr/PI_$new_directory.cfg
+			NP=10
+		;;
+		10) 	CP=$NP
+			exit 1;
+		;;
 	esac
 done
-
-### ADD FILES ###
-#read -p pause
-#
-
-### DELETE FILES ###
