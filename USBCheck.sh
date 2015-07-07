3b1607fecf8469bd8d6d7ea30601a0068200b467
Josh Williams <joshdont@gmail.com>
2015-07-07 00:47:55 -0400

    Updated comments describing file purpose

--- USBCheck.sh
+++ USBCheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #get the device number after unplugging/replugging in a device.
+#Temporary file. Long term this should be part of a script that makes it easier for people to add
+#multiple scanners of the same make/model/serial # to their Pi.
 
 read -p "unplug your device, hit a key when you are ready"
 find /sys/devices/ -name devnum -exec cat {} \; > check1.txt
