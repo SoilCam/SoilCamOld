448a778f8e1b176bd7fede2d6db9b2ef98b5f886
Josh Williams <joshdont@gmail.com>
2015-07-07 00:39:02 -0400

    Added temporary USBCheck.sh script, used to see what libusb devnum the scanner is

---
+++ USBCheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#get the device number after unplugging/replugging in a device.
+
+read -p "unplug your device, hit a key when you are ready"
+find /sys/devices/ -name devnum -exec cat {} \; > check1.txt
+sleep 1
+read -p "plug in your device, hit a key when you are ready"
+find /sys/devices/ -name devnum -exec cat {} \; > check2.txt
+
+result=$(diff --unchanged-line-format= --old-line-format= --new-line-format='%L' check1.txt check2.txt)
+
+echo $result
+
+rm check1.txt
+rm check2.txt
