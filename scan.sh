690b27282a9a8da837f80efd5f4723f9135a60e0
Josh Williams <joshdont@gmail.com>
2015-07-13 23:40:34 -0400

    Removed sudo command from scan.sh script after adding user pi to group scanner

--- scan.sh
+++ scan.sh
@@ -7,11 +7,13 @@ file=${prefix}$(date -d "today" +"%Y%m%dT%H%M%S").jpg
 
 if [[ "$MultipleScanners" =  "Yes" ]]
 then
+	devpath=$(/sbin/udevadm info --query=all --path=$devpath | grep DEVNUM)
+	devpath="${devpath:10:3}"
 	echo "Scanning with $backend:libusb:001:$devpath"
-	sudo /usr/bin/scanimage -d $backend:libusb:001:$devpath --mode Color --format tiff --resolution $res | sudo /usr/bin/convert - $imgloc/$file
+	/usr/bin/scanimage -d $backend:libusb:001:$devpath --mode Color --format tiff --resolution $res | /usr/bin/convert - $imgloc/$file
 else
 	echo "Scanning generic"
-	sudo /usr/bin/scanimage -d $backend --mode Color --format tiff --resolution $res | sudo /usr/bin/convert - $imgloc/$file
+	/usr/bin/scanimage -d $backend --mode Color --format tiff --resolution $res | /usr/bin/convert - $imgloc/$file
 fi
 
 
