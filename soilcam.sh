fff8cdd15047d39b61f74a55cd29bc0495f6a454
Josh Williams <joshdont@gmail.com>
2016-04-22 14:49:30 -0400

    Added more success/fail checks, updated and uncommented echo feedback

--- soilcam.sh
+++ soilcam.sh
@@ -34,8 +34,6 @@ setup(){
 		else
 			continue
 		fi
-	echo "All image directories found. You will not see this message again unless image"
-	echo "directories are missing... Initiating scan process."
 	done
 }
 
@@ -66,10 +64,11 @@ usage(){
 goscango(){
 	setup
 	#start a scan at 300 DPI, save as JPG with date & time stamp
-#	echo "Make Scan Go"
+	echo "Starting Scan"
 	file=sc_$(date -d "today" +"%Y%m%dT%H%M%S").jpg
 	/usr/bin/scanimage --mode Color --format tiff --resolution 300 -x 210 -y 295 | /usr/bin/convert -flip -flop - $imgloc1/$file
 	if [[ $? -eq 0 ]]; then
+		echo "Scan completed, moving onto process the image"
 		processimages
 	else
 		echo "Could not scan. Is the scanner plugged in? Have you installed ImageMagick?"
@@ -78,20 +77,31 @@ goscango(){
 }
 
 processimages(){
-#	echo "Process image"
 	TheDate=$(date -d today  +%Y%m%d)
 	period="sc_$TheDate"
 #	get number of temp files already in temp dir
-	tfiles=$(ls -1 $imgloc2/temp_*.jpg | wc -l)
-#	move files from new directory to processing directory
-	mv $imgloc1/*.jpg $imgloc2/
+	if [ "$(ls -A $imgloc2)" ]; then
+		tfiles=$(ls -1 $imgloc2/temp_*.jpg | wc -l)
+		count=$tfiles
+		echo "Found $count previously processed files in temp directory"
+	else
+		echo "No files in temp directory, setting count to 0"
+		count=0
+	fi
+
+#	move files from new directory to processing directory, exit if no images found.
+	if mv $imgloc1/*.jpg $imgloc2/; then
+		echo "Image found to process, let's resize and timestamp!"
+	else
+		echo "No image found to process, exiting..."
+		exit 1
+	fi
 	cd $imgloc2
-	count=$tfiles
 	for file in $period*.jpg; do
 		counter=$(printf %04d $count);
 		ndate="${file:3:4}\/${file:7:2}\/${file:9:2}"
 		ntime="${file:12:2}:${file:14:2}:${file:16:2}"
-                convert $file -resize x1080 -crop 769x1080-1+0 - | convert -background '#0008'   \
+                if convert $file -resize x1080 -crop 769x1080-1+0 - | convert -background '#0008'   \
                         -gravity center \
                         -fill white     \
                         -size 400x30    \
@@ -103,14 +113,18 @@ processimages(){
                         +swap   \
                         -gravity south  \
                         -composite      \
-                     "temp_$counter".jpg
-                count=$(($count+1));
-		mv $file $imgloc3/
+                     "temp_$counter".jpg; then
+			echo "Success!"
+	                count=$(($count+1));
+			mv $file $imgloc3/
+		else
+			echo "Something broke..."
+		fi
         done
 #	file counts
-	pfiles=$(ls -1 $imgloc3/$period*.jpg | wc -l)
-	ofiles=$(ls -1 $imgloc1/$period*.jpg | wc -l)
-	tfiles=$(ls -1 $imgloc2/temp_*.jpg | wc -l)
+#	pfiles=$(ls -1 $imgloc3/$period*.jpg | wc -l)
+#	ofiles=$(ls -1 $imgloc1/$period*.jpg | wc -l)
+#	tfiles=$(ls -1 $imgloc2/temp_*.jpg | wc -l)
 #	echo "Orig: $ofiles Proc: $pfiles Temp: $tfiles"
 
 }
@@ -118,33 +132,40 @@ processimages(){
 processvideo(){
 #	This is designed to be run five minutes after midnight
 #	Combine all images from processing into a video
-#	echo "Process video"
+	echo "Processing a video"
 	TheDate=$(date -d yesterday  +%Y%m%d)
 	if mv $imgloc2/*.jpg $imgloc4/; then
+		echo "Found images to process into a video"
 		if avconv -y -r 30 -i $imgloc4/temp_%04d.jpg -r 30 -vcodec libx264 -crf 20 -g 15 $vidloc/sc_$TheDate.mp4; then
 			avconv -y -i $vidloc/sc_$TheDate.mp4 -f mpegts -c copy -bsf:v h264_mp4toannexb $vidloc/sc_$TheDate.mpeg.ts
 			sleep 1
 			rm $imgloc4/*.jpg
+			ip=$(hostname -I | cut -f1 -d' ')
+			echo ""
+			echo "Video sc_$TheDate.mp4 generated, check for it in $vidloc, or via web at: http://$ip/~pi/videos/"
 		else
+			echo "Something bad happened trying to process the ideo. Aborting."
 			exit 1
-#			echo "failed to process video"
 		fi
 	else
+		echo "No images found to process video"
 		exit 1
-#		echo "failed to find images?"
 	fi
 }
 processtempvideo(){
 #	This is designed to process videos from today, whenever you please
 #	It does NOT however move any files around. This is primarily used for testing purposes
-#	echo "Process video temporarily"
+	echo "Process temporary video. This file will be prefixed with a 't_'"
 	TheDate=$(date -d today +%Y%m%d)
 	if avconv -y -r 30 -i $imgloc2/temp_%04d.jpg -r 30 -vcodec libx264 -crf 20 -g 15 $vidloc/t_sc_$TheDate.mp4; then
-	avconv -y -i $vidloc/t_sc_$TheDate.mp4 -f mpegts -c copy -bsf:v h264_mp4toannexb $vidloc/t_sc_$TheDate.mpeg.ts
-	sleep 1
+		avconv -y -i $vidloc/t_sc_$TheDate.mp4 -f mpegts -c copy -bsf:v h264_mp4toannexb $vidloc/t_sc_$TheDate.mpeg.ts
+		sleep 1
+		ip=$(hostname -I | cut -f1 -d' ')
+		echo ""
+		echo "Video t_sc_$TheDate.mp4 generated, check for it in $vidloc, or via web at: http://$ip/~pi/videos/"
 	else
+		echo "Failed to process video"
 		exit 1
-#		echo "failed to process video"
 	fi
 }
 
